e1c087f723fb059f3c739fd3060b0ef39eab822e
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-10-20 00:55:52 +1100

    Add card sections to screen for projects and todos

--- src/screen.js
+++ src/screen.js
@@ -7,13 +7,24 @@ const Screen = ((app) => {
     const container = prependChild(body, 'div', 'page-container');
 
     const navbar = appendChild(container, 'div', 'navbar');
-    const sidebar = appendChild(container, 'div', 'sidebar');
-    const content = appendChild(container, 'div', 'content');
+    const sidebar = appendChild(container, 'div', 'sidebar crud-layout');
+    const content = appendChild(container, 'div', 'content crud-layout');
 
     // Create headers in each section
     appendChild(navbar, 'h2', 'section-header').textContent = 'Things To Do';
     appendChild(sidebar, 'h2', 'section-header').textContent = 'Projects';
     appendChild(content, 'h2', 'section-header').textContent = 'Todos';
+
+    // Create buttons in project and todo sections
+    const addProjectButton = appendChild(sidebar, 'button', 'add-project');
+    addProjectButton.textContent = 'New Project';
+
+    const addTodoButton = appendChild(content, 'button', 'add-todo');
+    addTodoButton.textContent = 'New Todo';
+
+    // Create div for cards in project and todo sections
+    appendChild(sidebar, 'div', 'cards');
+    appendChild(content, 'div', 'cards');
   };
 
   return { render_layout };
