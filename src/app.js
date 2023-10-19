4d00fd2f43cf68427c75e6e9db5b0dfbeb53a825
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-10-20 00:10:45 +1100

    Rename index.js to app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import './style.scss';
+import { Project } from './models';
+
+const App = (() => {
+  const projects = [];
+
+  const getProjects = () => projects;
+
+  const createProject = (title) => {
+    projects.append(Project(title));
+  };
+
+  const deleteProject = (index) => {
+    return projects.splice(index, 1);
+  };
+
+  return { getProjects, createProject, deleteProject };
+})();
